6345343278225463fed0bdbf9883f3f7422c1b81
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-05 17:11:35 +0530

    Word Character Counter Project

---
+++ word-character-counter/word-character-counter.js
@@ -0,0 +1,21 @@
+function startCount() {
+     countWords();
+     countCharacters();
+}
+
+function countWords() {
+     let lineArray = document.getElementById('text').value.split('\n');
+     let count = 0, wordArray;
+     lineArray.forEach(line => {
+          wordArray = line.split(" ");
+          wordArray.forEach(element => {
+               if (/[A-Za-z0-9]/.test(element)) count++;
+          });
+     });
+
+     document.getElementById('word-count').innerText = count;
+}
+
+function countCharacters() {
+     document.getElementById('character-count').innerText = document.getElementById('text').value.length;
+}
