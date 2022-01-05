4936693d5d8fafe985a36e6f5e1edc6401e26d18
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-05 17:08:20 +0530

    Age Calculator Project

---
+++ age-calculator/common-codes/footer-code.js
@@ -0,0 +1,43 @@
+document.getElementById('footer').innerHTML = `
+     <footer>
+          <div id="logo">
+               <a href="../age-calculator/age-calculator.html">age calculator</a>
+          </div>
+
+          <div class="vertical-line"></div>
+
+          <div id="links">
+               <p>Links</p>
+               <hr>
+               <ul>
+                    <li><a href="../about-us/about-us.html">About Us</a></li>
+                    <li><a href="../how-it-works/how-it-works.html">How it all Works?</a></li>
+                    <li><a href="../feedback-form/feedback-form.html">Leave Us a Feedback</a></li>
+               </ul>
+          </div>
+
+          <div class="vertical-line"></div>
+
+          <div id="contact-us">
+               <p>Contact Us</p>
+               <hr>
+               <div>
+                    <a href="mailto:programmerrohith@gmail.com">
+                         <i class="fas fa-envelope" id="email"></i>
+                    </a>
+                    <a href="https://github.com/rohithpala" target="_blank">
+                         <i class="fab fa-github" id="github"></i>
+                    </a>
+                    <a href="https://instagram.com/rohith._.pala._" target="_blank">
+                         <i class="fab fa-instagram" id="instagram"></i>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UC_HL-GS9SzPh3O7i71qX2Ww" target="_blank">
+                         <i class="fab fa-youtube" id="youtube"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/rohithpala" target="_blank">
+                         <i class="fab fa-linkedin" id="linkedin"></i>
+                    </a>
+               </div>
+          </div>
+     </footer>
+`;
