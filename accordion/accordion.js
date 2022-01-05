6365781cef316483411a88360238e469014fb5d9
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-05 17:08:09 +0530

    Accordion

---
+++ accordion/accordion.js
@@ -0,0 +1,8 @@
+function showPanel(id) {
+   const panel = document.getElementById(id + "-panel");
+   if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+   } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+   }
+}
