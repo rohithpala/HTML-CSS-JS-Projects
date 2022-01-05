8d317686b169f9ff1cd63898ad7e43dd08a844e7
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-05 17:08:53 +0530

    Bulb on/off Project

---
+++ bulb-on-off/bulb-on-off.js
@@ -0,0 +1,10 @@
+function changeValues() {
+     const button = document.getElementById('bulb-button');
+     if (button.innerText === 'Turn on the light') {
+          document.getElementById('bulb-image').src = 'bulbon.gif';
+          button.innerText = 'Turn off the light';
+     } else if (button.innerText === 'Turn off the light') {
+          document.getElementById('bulb-image').src = 'bulboff.gif';
+          button.innerText = 'Turn on the light';
+     }
+}
