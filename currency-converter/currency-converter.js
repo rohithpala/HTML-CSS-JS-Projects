55fc859fb050d7d82b796830cf0fedaa4bd1e983
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-05 17:09:47 +0530

    Currency Converter Project

---
+++ currency-converter/currency-converter.js
@@ -0,0 +1,12 @@
+const fromSelect = document.getElementById("from");
+const toSelect = document.getElementById("to");
+const from_ = document.getElementById('from-input');
+const to = document.getElementById('to-input');
+
+from_.addEventListener("input", convertCurrency);
+
+function convertCurrency() {
+   const from = from_.value;
+
+   from2to(fromSelect.getSelectedItem(), toSelect.getSelectedItem());
+}
