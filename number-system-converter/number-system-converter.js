08eb028b0ed9582983b665d9f88b07603cefc19f
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-07 15:13:38 +0530

    Number System Converter

--- number-system-converter/number-system-converter.js
+++ number-system-converter/number-system-converter.js
@@ -2,6 +2,7 @@ const fromSelected = document.getElementById("from-select");
 const toSelected = document.getElementById("to-select");
 const from = document.getElementById("from-input");
 const to = document.getElementById("to-input");
+const error = document.getElementById('error');
 
 let fromNS = "Binary", toNS = "Binary";
 
@@ -15,16 +16,16 @@ toSelected.addEventListener("change", function () {
    to.placeholder = toNS + " Number";
 });
 
-from.addEventListener("change", function () {
-   from.style.backgroundColor = "#fff";
+from.addEventListener("input", function () {
+   error.style.display = "none";
 });
 
 let fromValue;
 document.getElementById("convert-button").addEventListener("click", function () {
    switch (fromNS) {
       case "Binary":
-         if (/^[01]*$/.test(from.value)) {
-            fromValue = from.value;
+         fromValue = from.value;
+         if (/^[01]*$/.test(fromValue)) {
             switch (toNS) {
                case "Decimal": to.value = parseInt(fromValue, 2);
                   break;
@@ -35,43 +36,66 @@ document.getElementById("convert-button").addEventListener("click", function () 
                default: to.value = fromValue;
             }
          } else {
-            from.style.backgroundColor = "#f00";
+            error.style.display = "inherit";
+            error.innerText = "Invalid " + fromNS + " Number";
+            to.value = "";
          }
          break;
+
       case "Decimal":
          fromValue = from.value;
-         switch (toNS) {
-            case "Binary": to.value = Math.abs(fromValue).toString(2);
-               break;
-            case "Hexadecimal": to.value = Math.abs(fromValue).toString(16).toUpperCase();
-               break;
-            case "Octal": to.value = Math.abs(fromValue).toString(8);
-               break;
-            default: to.value = fromValue;
+         if (/^[0-9]*$/.test(fromValue)) {
+            switch (toNS) {
+               case "Binary": to.value = Math.abs(fromValue).toString(2);
+                  break;
+               case "Hexadecimal": to.value = Math.abs(fromValue).toString(16).toUpperCase();
+                  break;
+               case "Octal": to.value = Math.abs(fromValue).toString(8);
+                  break;
+               default: to.value = fromValue;
+            }
+         } else {
+            error.style.display = "inherit";
+            error.innerText = "Invalid " + fromNS + " Number";
+            to.value = "";
          }
          break;
+
       case "Hexadecimal":
          fromValue = from.value;
-         switch (toNS) {
-            case "Binary": to.value = parseInt(fromValue, 16).toString(2);
-               break;
-            case "Decimal": to.value = parseInt(fromValue, 16);
-               break;
-            case "Octal": to.value = parseInt(fromValue, 16).toString(8);
-               break;
-            default: to.value = fromValue;
+         if (/^[0-9a-fA-F]*$/.test(fromValue)) {
+            switch (toNS) {
+               case "Binary": to.value = parseInt(fromValue, 16).toString(2);
+                  break;
+               case "Decimal": to.value = parseInt(fromValue, 16);
+                  break;
+               case "Octal": to.value = parseInt(fromValue, 16).toString(8);
+                  break;
+               default: to.value = fromValue;
+            }
+         } else {
+            error.style.display = "inherit";
+            error.innerText = "Invalid " + fromNS + " Number";
+            to.value = "";
          }
          break;
+
       case "Octal":
          fromValue = from.value;
-         switch (toNS) {
-            case "Binary": to.value = parseInt(fromValue, 8).toString(2);
-               break;
-            case "Decimal": to.value = parseInt(fromValue, 8);
-               break;
-            case "Hexadecimal": to.value = parseInt(fromValue, 8).toString(16).toUpperCase();
-               break;
-            default: to.value = fromValue;
+         if (/^[0-7]*$/.test(fromValue)) {
+            switch (toNS) {
+               case "Binary": to.value = parseInt(fromValue, 8).toString(2);
+                  break;
+               case "Decimal": to.value = parseInt(fromValue, 8);
+                  break;
+               case "Hexadecimal": to.value = parseInt(fromValue, 8).toString(16).toUpperCase();
+                  break;
+               default: to.value = fromValue;
+            }
+         } else {
+            error.style.display = "inherit";
+            error.innerText = "Invalid " + fromNS + " Number";
+            to.value = "";
          }
          break;
    }
