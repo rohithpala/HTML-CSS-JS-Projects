000a36a934b335d416a8fbad6f80cf4440bc5d24
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-13 15:21:01 +0530

    Color Picker done in HTML CSS & JS

--- color-picker/color-picker.js
+++ color-picker/color-picker.js
@@ -1,75 +1,75 @@
-const rRange = document.getElementById('r-range');
-const rNumber = document.getElementById('r-number');
-const gRange = document.getElementById('g-range');
-const gNumber = document.getElementById('g-number');
-const bRange = document.getElementById('b-range');
-const bNumber = document.getElementById('b-number');
-const hex = document.getElementById('hex');
-const colorPanel = document.getElementById('color');
+const rRange = document.getElementById("r-range");
+const rNumber = document.getElementById("r-number");
+const gRange = document.getElementById("g-range");
+const gNumber = document.getElementById("g-number");
+const bRange = document.getElementById("b-range");
+const bNumber = document.getElementById("b-number");
+const hex = document.getElementById("hex");
+const colorPanel = document.getElementById("color");
 
 // syncs the number input and range input
 function syncRGBHexValues(color_name, value_) {
-     if (color_name !== "hex") {
-          document.getElementById(color_name + '-number').value =
-               document.getElementById(color_name + '-range').value = value_;
+   if (color_name !== "hex") {
+      document.getElementById(color_name + "-number").value =
+         document.getElementById(color_name + "-range").value = value_;
 
-          setColor(color_name);
-     } else { // compare old and new hex values and change the corresponding ranges : TODO
-          if (hex.value.length === 0) {
-               colorPanel.style.backgroundColor = "#ffffff";
-               rRange.style.backgroundColor = "#ff0000";
-               gRange.style.backgroundColor = "#00ff00";
-               bRange.style.backgroundColor = "#0000ff";
+      setColor(color_name);
+   } else {
+      if (hex.value.length === 0) {
+         colorPanel.style.backgroundColor = "#ffffff";
+         rRange.style.backgroundColor = "#ff0000";
+         gRange.style.backgroundColor = "#00ff00";
+         bRange.style.backgroundColor = "#0000ff";
 
-               rRange.value = gRange.value = bRange.value = 255;
-               rNumber.value = gNumber.value = bNumber.value = 255;
-          } else if (hex.value.length === 6) {
-               setColor(color_name);
-          }
-     }
+         rRange.value = gRange.value = bRange.value = 255;
+         rNumber.value = gNumber.value = bNumber.value = 255;
+      } else if (hex.value.length === 6) {
+         setColor(color_name);
+      }
+   }
 }
 
 // converts rgb values to hex values
 function rgb2hex(r, g, b) {
-     const rgb = (r << 16) | (g << 8) | b;
-     return rgb.toString(16).padStart(6, 0);
+   const rgb = (r << 16) | (g << 8) | b;
+   return rgb.toString(16).padStart(6, 0);
 }
 
 // converts hex values to rgb values
 function hex2rgb(hex) {
-     var result = [hex.substring(0, 2), hex.substring(2, 4), hex.substring(4)];
-     return { r: parseInt(result[0], 16), g: parseInt(result[1], 16), b: parseInt(result[2], 16) };
+   var result = [hex.substring(0, 2), hex.substring(2, 4), hex.substring(4)];
+   return { r: parseInt(result[0], 16), g: parseInt(result[1], 16), b: parseInt(result[2], 16) };
 }
 
 // sets color to color holder and to ranges
 function setColor(color_name) {
-     if (color_name !== "hex") {
-          hex.value = rgb2hex(rRange.value, gRange.value, bRange.value);
-     } else {
-          const rgb = hex2rgb(hex.value);
-          rRange.value = rNumber.value = rgb.r;
-          gRange.value = gNumber.value = rgb.g;
-          bRange.value = bNumber.value = rgb.b;
-     }
-     colorPanel.style.backgroundColor = "#" + hex.value;
+   if (color_name !== "hex") {
+      hex.value = rgb2hex(rRange.value, gRange.value, bRange.value);
+   } else {
+      const rgb = hex2rgb(hex.value);
+      rRange.value = rNumber.value = rgb.r;
+      gRange.value = gNumber.value = rgb.g;
+      bRange.value = bNumber.value = rgb.b;
+   }
+   colorPanel.style.backgroundColor = "#" + hex.value;
 
-     rRange.style.background = "rgb(" + rRange.value + ", 0, 0)";
-     gRange.style.background = "rgb(0, " + gRange.value + ", 0)";
-     bRange.style.background = "rgb(0 , 0, " + bRange.value + ")";
+   rRange.style.background = "rgb(" + rRange.value + ", 0, 0)";
+   gRange.style.background = "rgb(0, " + gRange.value + ", 0)";
+   bRange.style.background = "rgb(0 , 0, " + bRange.value + ")";
 }
 
 function copyToClipboard(text) {
-     if (text === "HEX") {
-          hex.select();
-          hex.setSelectionRange(0, 99999);
+   if (text === "HEX") {
+      hex.select();
+      hex.setSelectionRange(0, 99999);
 
-          navigator.clipboard.writeText("#" + hex.value);
-     } else {
-          rNumber.select();
-          gNumber.select();
-          bNumber.select();
+      navigator.clipboard.writeText("#" + hex.value);
+   } else {
+      rNumber.select();
+      gNumber.select();
+      bNumber.select();
 
-          navigator.clipboard.writeText("rgb(" + [rNumber.value, gNumber.value, bNumber.value].join(', ') + ")");
-     }
-     alert("Copied the " + text + " value to Clipboard");
+      navigator.clipboard.writeText("rgb(" + [rNumber.value, gNumber.value, bNumber.value].join(", ") + ")");
+   }
+   alert("Copied the " + text + " value to Clipboard");
 }
