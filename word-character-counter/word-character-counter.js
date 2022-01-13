312072329f5ff13f9c0334bea8d9f1d9352159c8
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-13 15:25:51 +0530

    Word Character Counter done using JS

--- word-character-counter/word-character-counter.js
+++ word-character-counter/word-character-counter.js
@@ -1,21 +1,21 @@
 function startCount() {
-     countWords();
-     countCharacters();
+   countWords();
+   countCharacters();
 }
 
 function countWords() {
-     let lineArray = document.getElementById('text').value.split('\n');
-     let count = 0, wordArray;
-     lineArray.forEach(line => {
-          wordArray = line.split(" ");
-          wordArray.forEach(element => {
-               if (/[A-Za-z0-9]/.test(element)) count++;
-          });
-     });
+   let lineArray = document.getElementById("text").value.split("\n");
+   let count = 0, wordArray;
+   lineArray.forEach(line => {
+      wordArray = line.split(" ");
+      wordArray.forEach(element => {
+         if (/[A-Za-z0-9]/.test(element)) count++;
+      });
+   });
 
-     document.getElementById('word-count').innerText = count;
+   document.getElementById("word-count").innerText = count;
 }
 
 function countCharacters() {
-     document.getElementById('character-count').innerText = document.getElementById('text').value.length;
+   document.getElementById("character-count").innerText = document.getElementById("text").value.length;
 }
