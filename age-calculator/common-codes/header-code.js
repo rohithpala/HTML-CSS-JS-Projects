ba481775a790635ebfc12444ec82cb59dc3c9123
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-13 15:13:29 +0530

    Calculating Age based on DOB done uing JS

--- age-calculator/common-codes/header-code.js
+++ age-calculator/common-codes/header-code.js
@@ -1,10 +1,10 @@
-document.getElementById('header').innerHTML = `
-     <header>
-          <ul id="menu">
-               <li><a href="../age-calculator/age-calculator.html">Home</a></li>
-               <li><a href="../about-us/about-us.html">About Us</a></li>
-               <li><a href="../how-it-works/how-it-works.html">How it all Works?</a></li>
-               <li><a href="../feedback-form/feedback-form.html">Leave Us a Feedback</a></li>
-          </ul>
-     </header>
+document.getElementById("header").innerHTML = `
+   <header>
+      <ul id="menu">
+         <li><a href="../age-calculator/age-calculator.html">Home</a></li>
+         <li><a href="../about-us/about-us.html">About Us</a></li>
+         <li><a href="../how-it-works/how-it-works.html">How it all Works?</a></li>
+         <li><a href="../feedback-form/feedback-form.html">Leave Us a Feedback</a></li>
+      </ul>
+   </header>
 `;
