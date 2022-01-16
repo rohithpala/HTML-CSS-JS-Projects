213f002a5f182206b5e40f0d827dbd96e36d97cf
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-16 19:54:56 +0530

    Counter done using JS with Buttons Option Added

--- counter/counter.js
+++ counter/counter.js
@@ -1,28 +1,49 @@
+const counter = document.getElementById("counter");
+
+const decrementButton = document.getElementById("decrement");
+const resetButton = document.getElementById("reset");
+const incrementButton = document.getElementById("increment");
+
+decrementButton.addEventListener("click", decrement);
+resetButton.addEventListener("click", reset);
+incrementButton.addEventListener("click", increment);
+
+decrementButton.addEventListener("transitionend", removeTransformProperty);
+resetButton.addEventListener("transitionend", removeTransformProperty);
+incrementButton.addEventListener("transitionend", removeTransformProperty);
+
+window.addEventListener("keydown", function (e) {
+   const keyPressed = e.key;
+   if (keyPressed === "+") increment();
+   else if (keyPressed === "-") decrement();
+   else if (keyPressed === "r" || keyPressed === "R" || keyPressed === "0") reset();
+});
+
 function decrement() {
-   const counter = document.getElementById("counter");
+   decrementButton.classList.add("pressed");
    counter.innerText = parseInt(counter.innerText) - 1;
    changeColor();
 }
 
 function reset() {
-   document.getElementById("counter").innerText = 0;
+   resetButton.classList.add("pressed");
+   counter.innerText = 0;
    changeColor();
 }
 
 function increment() {
-   const counter = document.getElementById("counter");
+   incrementButton.classList.add("pressed");
    counter.innerText = parseInt(counter.innerText) + 1;
    changeColor();
 }
 
 function changeColor() {
-   const counter = document.getElementById("counter");
    const counterValue = parseInt(counter.innerText);
-   if (counterValue < 0) {
-      counter.style.color = "#f00";
-   } else if (counterValue === 0) {
-      counter.style.color = "#000";
-   } else {
-      counter.style.color = "#006400";
-   }
+   if (counterValue < 0) counter.style.color = "#f00";
+   else if (counterValue === 0) counter.style.color = "#000";
+   else counter.style.color = "#006400";
+}
+
+function removeTransformProperty(e) {
+   e.target.classList.remove("pressed");
 }
