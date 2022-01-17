8b2d3381b7899e82330308ebb09258b6c1f0f53f
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-17 22:53:01 +0530

    Creating an Accordion in using HTML CSS & JS

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -1,8 +1,5 @@
 function showPanel(id) {
    const panel = document.getElementById(id + "-panel");
-   if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-   } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-   }
+   if (panel.style.maxHeight) panel.style.maxHeight = null;
+   else panel.style.maxHeight = panel.scrollHeight + "px";
 }
