e8b5cb5a2d05ac0dedb4b5f749976bccdb532deb
rohithpala <rohithpala02@gmail.com>
2022-01-27 10:36:29 +0530

    Password Checker Project (UI 2)

--- password-checker-ui2/password-checker.js
+++ password-checker-ui2/password-checker.js
@@ -1,8 +1,10 @@
 const input = document.querySelector("input");
-const showHide = document.getElementsByClassName("show-hide")[0];
+const showHide = document.querySelector(".show-hide");
 const strengthContainer = document.querySelector(".strength-container");
 const indicator = document.querySelector(".indicator");
 const strength = document.querySelector(".strength");
+
+window.addEventListener("load", input.focus());
 
 input.addEventListener("input", function () {
    strengthContainer.classList.add("active");
@@ -14,18 +16,16 @@ input.addEventListener("input", function () {
       const alphabetMatch = password.match(/[a-zA-Z]/);
       const digitsMatch = password.match(/[0-9]/);
       const splCharactersMatch = password.match(/[!,@,#,$,%,^,&,*,?,_,(,),-,+,=,~]/);
-      const indicatorClassList = indicator.classList;
+      const indicator_ = indicator.children[0];
 
       if (alphabetMatch && digitsMatch && splCharactersMatch && password.length >= 8) {
          strength.innerText = "Password is Strong";
-         input.style.borderColor = showHide.style.color = indicator.style.color = strength.style.color = "#0f0";
-         indicatorClassList.remove("fa-exclamation-circle");
-         indicatorClassList.add("fa-check-circle");
+         input.style.borderColor = indicator.style.color = strength.style.color = "#0f0";
+         indicator_.classList.replace("fa-exclamation-circle", "fa-check-circle");
       } else if (alphabetMatch || digitsMatch || splCharactersMatch) {
          strength.innerText = "Password is Weak";
-         input.style.borderColor = showHide.style.color = indicator.style.color = strength.style.color = "#ff6333";
-         indicatorClassList.remove("fa-check-circle");
-         indicatorClassList.add("fa-exclamation-circle");
+         input.style.borderColor = indicator.style.color = strength.style.color = "#ff6333";
+         indicator_.classList.replace("fa-check-circle", "fa-exclamation-circle");
       }
    }
 });
@@ -34,9 +34,11 @@ showHide.addEventListener("click", function () {
    if (input.type === "password") {
       console.log(1);
       input.type = "text";
-      showHide.classList.replace("fa-eye-slash", "fa-eye");
+      showHide.children[0].classList.replace("fa-eye-slash", "fa-eye");
    } else {
+      console.log(0);
       input.type = "password";
-      showHide.classList.replace("fa-eye", "fa-eye-slash");
+      showHide.children[0].classList.replace("fa-eye", "fa-eye-slash");
    }
+   console.log(showHide.children[0].classList);
 });
