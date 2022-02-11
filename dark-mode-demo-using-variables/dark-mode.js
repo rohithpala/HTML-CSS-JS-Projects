be278da44f41f634d70f1b4db7d62e75e0409e7e
rohithpala <rohithpala02@gmail.com>
2022-02-11 10:04:14 +0530

    Demonstrating Dark Mode in HTML CSS & JS

--- dark-mode-demo-using-variables/dark-mode.js
+++ dark-mode-demo-using-variables/dark-mode.js
@@ -2,10 +2,10 @@ document.getElementById("dark-mode").addEventListener("input", function () {
    const r = document.querySelector(":root");
    const cb = document.getElementById("dark-mode");
    if (cb.checked) {
-      r.style.setProperty("--bg-color", "#000");
+      r.style.setProperty("--bg-color", "#121212");
       r.style.setProperty("--text-color", "#fff");
    } else {
       r.style.setProperty("--bg-color", "#fff");
-      r.style.setProperty("--text-color", "#000");
+      r.style.setProperty("--text-color", "#121212");
    }
 });
