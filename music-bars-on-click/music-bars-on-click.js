8dc2354191da7a5c20dfb367a15457ddb5fd3683
rohithpala <rohithpala02@gmail.com>
2022-08-24 21:39:06 +0530

    Get music bars from bottom to where you click

--- music-bars-on-click/music-bars-on-click.js
+++ music-bars-on-click/music-bars-on-click.js
@@ -1,3 +1,9 @@
-document.onclick = function(event) {
-   console.log(event.clientX, event.clientY);
+document.onclick = (event) => {
+   const musicBar = document.createElement("div");
+   musicBar.className = "music-bar";
+   document.body.appendChild(musicBar);
+
+   musicBar.style.left = (event.clientX - 3) + "px";
+   musicBar.style.top = event.clientY + "px";
+   musicBar.style.height = (735 - event.clientY) + "px";
 };
