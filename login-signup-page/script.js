4e4979a2120af8c4eaa2b4b4125c4ce70726fa68
rohithpala <rohithpala02@gmail.com>
2022-09-03 14:54:53 +0530

    Login Signup Page

---
+++ login-signup-page/script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector(".container");
+const showHidePw = document.getElementsByClassName("showHidePw");
+const pwFields = document.querySelector(".password");
+
+console.log(Array.from(showHidePw));
+
+// showHidePw.forEach(icon => {
+//    icon.addEventListener("click", () => {
+//       pwFields.forEach(pwField => {
+//          if (pwField.type === "password") {
+//             pwField.type = "text";
+//             icon.classList.add("uil-eye-slash");
+//             icon.classList.remove("uil-eye");
+//          } else {
+//             pwField.type = "password";
+//             icon.classList.add("uil-eye");
+//             icon.classList.remove("uil-eye-slash");
+//          }
+//       });
+//    });
+// });
