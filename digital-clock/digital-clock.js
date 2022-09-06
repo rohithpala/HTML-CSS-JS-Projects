5c4020e98bd349f27da514b1b9f848dac2ff676b
rohithpala <rohithpala02@gmail.com>
2022-09-06 23:05:33 +0530

    Digital Clock using HTML, CSS & JS

--- digital-clock/digital-clock.js
+++ digital-clock/digital-clock.js
@@ -1,29 +1,44 @@
-const hours = document.querySelector(".hours");
-const minutes = document.querySelector(".minutes");
-const seconds = document.querySelector(".seconds");
+const hoursElement = document.querySelector(".hours");
+const minutesElement = document.querySelector(".minutes");
+const secondsElement = document.querySelector(".seconds");
 const period = document.querySelector(".period");
+
+const hours24Element = document.querySelector(".hours-24");
+const minutes24Element = document.querySelector(".minutes-24");
+const seconds24Element = document.querySelector(".seconds-24");
+
+let hours, minutes, seconds;
 let date, count = 0;
 
 setInterval(setClock, 1000);
 
 function setClock() {
    date = new Date();
+   hours = date.getHours().toString().padStart(2, "0");
+   minutes = date.getMinutes().toString().padStart(2, "0");
+   seconds = date.getSeconds().toString().padStart(2, "0");
 
-   seconds.innerText = date.getSeconds().toString().padStart(2, "0");
+   // Setting 24 Hour Clock
+   hours24Element.innerText = hours;
+   minutes24Element.innerText = minutes;
+   seconds24Element.innerText = seconds;
 
-   if (seconds.innerText === "00" || count === 0) {
-      minutes.innerText = date.getMinutes().toString().padStart(2, "0");
+   // Setting 12 Hour Clock
+   secondsElement.innerText = seconds;
 
-      if (minutes.innerText === "00" || count === 0) {
+   if (secondsElement.innerText === "00" || count === 0) {
+      minutesElement.innerText = date.getMinutes().toString().padStart(2, "0");
+
+      if (minutesElement.innerText === "00" || count === 0) {
          count++;
          if (date.getHours() > 12) {
-            hours.innerText = (date.getHours() - 12).toString().padStart(2, "0");
+            hoursElement.innerText = (date.getHours() - 12).toString().padStart(2, "0");
             if (period.innerText === "A.M.")
                period.innerText = "P.M.";
          } else {
             if (date.getHours() === 0)
-               hours.innerText = "12";
-            hours.innerText = date.getHours().toString().padStart(2, "0");
+               hoursElement.innerText = "12";
+            hoursElement.innerText = date.getHours().toString().padStart(2, "0");
             if (period.innerText === "P.M.")
                period.innerText = "A.M.";
          }
