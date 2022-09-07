a8ff99f9aea8f52e4b1ca135e5ae69885f7b9534
rohithpala <rohithpala02@gmail.com>
2022-09-07 19:16:05 +0530

    Guess the Number game done in HTML, CSS & JS

--- guess-the-number/guess-the-number.js
+++ guess-the-number/guess-the-number.js
@@ -1,8 +1,24 @@
+const minElement = document.getElementById("minimum");
+const maxElement = document.getElementById("maximum");
+
+const guessedNumConatinerStyle = document.getElementById("guessed-number-container").style;
+const yourTurnTextStyle = document.getElementById("your-turn-text").style;
+const goButtonStyle = document.getElementById("go").style;
+
+const userConatinerStyle = document.getElementById("user-container").style;
+const chancesLeft = document.getElementById("chances-left");
+const chances = document.getElementById("chances");
+const userInput = document.getElementById("user-number-input");
+const previousGuesses = document.getElementById("previous-guesses");
+const submitButtonStyle = document.getElementById("submit-button").style;
+const hint = document.getElementById("hint");
+const startOverStyle = document.getElementById("start-over").style;
+
 let number;
 
 function startGame() {
-   let minimum = document.getElementById("minimum").value;
-   let maximum = document.getElementById("maximum").value;
+   let minimum = minElement.value;
+   let maximum = maxElement.value;
 
    if (minimum === "" && maximum === "") {
       alert("Please input the minimum and maximum values");
@@ -20,7 +36,6 @@ function startGame() {
          alert("Minimum value cannot be equal to Maximum value");
       } else {
          number = Math.floor(Math.random() * (maximum - minimum)) + minimum;
-         const userInput = document.getElementById("user-number-input");
          userInput.min = minimum;
          userInput.max = maximum;
          showMessage();
@@ -29,87 +44,84 @@ function startGame() {
 }
 
 function showMessage() {
-   document.getElementById("guessed-number-container").style.display = "flex";
+   guessedNumConatinerStyle.display = "flex";
    setTimeout(function () {
-      document.getElementById("your-turn-text").style.display = "inherit";
+      yourTurnTextStyle.display = "inherit";
    }, 1000);
    setTimeout(function () {
-      document.getElementById("go").style.display = "inherit";
+      goButtonStyle.display = "inherit";
    }, 2000);
 }
 
 function showUserContainer() {
-   gncStyle = document.getElementById("guessed-number-container").style;
-   gncStyle.border = "2px solid #000";
-   gncStyle.opacity = "0.5";
-   gncStyle.pointerEvents = "none";
-   gncStyle.userSelect = "none";
+   guessedNumConatinerStyle.border = "2px solid #000";
+   guessedNumConatinerStyle.opacity = "0.5";
+   guessedNumConatinerStyle.pointerEvents = "none";
+   guessedNumConatinerStyle.userSelect = "none";
 
-   ucStyle = document.getElementById("user-container").style;
-   ucStyle.display = "flex";
-   ucStyle.border = "2px solid #fff";
+   userConatinerStyle.display = "flex";
+   userConatinerStyle.border = "2px solid #fff";
 
-   document.getElementById("chances-left").innerText = document.getElementById("chances").innerText;
+   chancesLeft.innerText = chances.innerText;
 }
 
+let guessedNumber, guessed = false;
 function checkTheGuess() {
-   let guessedNumber = document.getElementById("user-number-input").value;
+   guessedNumber = userInput.value;
    if (guessedNumber === "") {
       alert("Please type a number first");
    } else {
       guessedNumber = parseInt(guessedNumber);
-      const chancesLeftText = document.getElementById("chances-left");
-      const hint = document.getElementById("hint");
       hint.style.display = "inherit";
-      hint.style.marginTop = "10px";
 
-      let guessed = false;
       if (guessedNumber < number) {
-         hint.innerText = "Your Guess is LESS than the Number";
+         hint.innerHTML = `Your Guess is <pre style="color: #f00"> LESS </pre> than the Number`;
       } else if (guessedNumber > number) {
-         hint.innerText = "Your Guess is GREATER than the Number";
+         hint.innerHTML = `Your Guess is <pre style="color: #f00"> GREATER </pre> than the Number`;
       } else {
          hint.innerText = "Hurray! You GUESSED it";
          hint.style.backgroundColor = "#0f0";
          guessed = true;
       }
 
-      if (guessed || chancesLeftText.innerText === "0") {
-         if (chancesLeftText.innerText === "0") {
+      if (guessed || chancesLeft.innerText === "0") {
+         userInput.disabled = true;
+         submitButtonStyle.pointerEvents = "none";
+         startOverStyle.display = "inherit";
+      } else {
+         previousGuesses.innerText += (previousGuesses.innerText === "" ? "" : ", ") + guessedNumber;
+         chancesLeft.innerText = parseInt(chancesLeft.innerText) - 1;
+         
+         if (chancesLeft.innerText === "0") {
             hint.style.backgroundColor = "#f00";
             hint.innerText = "You lost. The number was: " + number;
+            userInput.disabled = true;
+            submitButtonStyle.pointerEvents = "none";
+            startOverStyle.display = "inherit";
          }
-         document.getElementById("user-number-input").disabled = true;
-         document.getElementById("submit-button").style.pointerEvents = "none";
-         document.getElementById("start-over").style.display = "inherit";
-      } else {
-         document.getElementById("previous-guesses").innerText += " " + guessedNumber;
-         chancesLeftText.innerText = parseInt(chancesLeftText.innerText) - 1;
       }
    }
 }
 
 function startOver() {
-   gncStyle = document.getElementById("guessed-number-container").style;
-   gncStyle.display = "none";
-   gncStyle.border = "none";
-   gncStyle.opacity = "1";
-   gncStyle.pointerEvents = "auto";
+   guessedNumConatinerStyle.display = "none";
+   guessedNumConatinerStyle.border = "none";
+   guessedNumConatinerStyle.opacity = "1";
+   guessedNumConatinerStyle.pointerEvents = "auto";
 
-   document.getElementById("your-turn-text").style.display = "none";
-   document.getElementById("go").style.display = "none";
-   document.getElementById("user-container").style.display = "none";
-   document.getElementById("start-over").style.display = "none";
+   yourTurnTextStyle.display = "none";
+   goButtonStyle.display = "none";
+   userConatinerStyle.display = "none";
+   startOverStyle.display = "none";
 
-   document.getElementById("user-number-input").disabled = false;
+   userInput.disabled = false;
 
-   document.getElementById("minimum").value = document.getElementById("maximum").value = document.getElementById("user-number-input").value = "";
+   minElement.value = maxElement.value = userInput.value = "";
 
-   document.getElementById("submit-button").style.pointerEvents = "auto";
+   submitButtonStyle.pointerEvents = "auto";
 
-   document.getElementById("previous-guesses").innerText = "";
+   previousGuesses.innerText = "";
 
-   const hintStyle = document.getElementById("hint").style;
-   hintStyle.backgroundColor = "#07a";
-   hintStyle.display = "none";
+   hint.style.backgroundColor = "#07a";
+   hint.style.display = "none";
 }
