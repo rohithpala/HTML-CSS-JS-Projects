29ee55a19a328081b970859ba7266ce58d1d1745
rohithpala <rohithpala02@gmail.com>
2022-09-15 23:07:56 +0530

    Horizontal Progress Bar with Value

---
+++ progress-bars/horizontal-pb/horizontal-pb.js
@@ -0,0 +1,27 @@
+const progressElement = document.querySelector(".progress");
+const progresValueElement = document.querySelector(".progress-value");
+let width = 0, progressInterval;
+let startBtn = document.querySelector(".start-btn");
+let stopBtn = document.querySelector(".stop-btn");
+
+function frame() {
+   if (width >= 100) {
+      clearInterval(progressInterval);
+      startBtn.innerText = "Restart";
+   } else {
+      width++;
+      progressElement.style.width = width + "%";
+      progresValueElement.innerText = width + "%";
+   }
+}
+
+startBtn.onclick = function () {
+   if (startBtn.innerText !== "Resume")
+      width = 0;
+   progressInterval = setInterval(frame, 50);
+};
+
+stopBtn.onclick = function () {
+   clearInterval(progressInterval);
+   startBtn.innerText = "Resume";
+};
