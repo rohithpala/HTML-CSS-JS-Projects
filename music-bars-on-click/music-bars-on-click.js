edfa436a78eba943f98f7fb43f88862391938fde
rohithpala <rohithpala02@gmail.com>
2022-09-16 18:18:14 +0530

    Get music bars from bottom to your click position

--- music-bars-on-click/music-bars-on-click.js
+++ music-bars-on-click/music-bars-on-click.js
@@ -1,7 +1,14 @@
 const DEVICE_HEIGHT = 1000; // to start from bottom of the screen
 const TRANSITION_TIME = "1";
 
+// let context = new AudioContext();
+
 document.onclick = (event) => {
+   // let o = context.createOscillator();
+   // let g = context.createGain();
+   // o.connect(g);
+   // g.connect(context.destination);
+
    let musicBar = document.createElement("div");
 
    musicBar.style.position = "absolute";
@@ -17,12 +24,16 @@ document.onclick = (event) => {
       musicBar.style.left = (event.clientX - 4) + "px";
       musicBar.style.top = event.clientY + "px";
       musicBar.style.height = (DEVICE_HEIGHT - event.clientY) + "px";
+      // o.frequency.value = event.clientY;
+      // o.start();
    }, 1); // 1ms for allowing to apply default values, so that transition could take place
+
 
    // This makes the bar go down
    setTimeout(() => {
       musicBar.style.top = "100vh";
       musicBar.style.height = "0";
+      // o.stop();
    }, TRANSITION_TIME * 1000);
 
    // After the transition is completed, we can remove the element
