ca7ef88a027cb5fca6e8f1834f14529d6220270d
rohithpala <rohithpala02@gmail.com>
2022-10-06 14:49:39 +0530

    Notes App

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,12 +1,15 @@
 const addButton = document.querySelector(".add-btn");
 const addNoteContainer = document.querySelector(".add-note-container");
 const addNoteForm = document.querySelector(".add-note-form");
+const titleError = document.querySelector(".title-error");
+const bodyError = document.querySelector(".body-error");
 const addNoteCloseButton = document.querySelector(".close-btn");
 const notesConatiner = document.querySelector(".notes");
 const noNotes = document.querySelector(".no-notes");
 
-let notes, noteID = 0;
+let notes, noteID = 0, title, body;
 
+// Loading all the notes from local stoarge
 window.onload = () => {
    notes = JSON.parse(localStorage.getItem("notes")) === null ? [] : JSON.parse(localStorage.getItem("notes"));
    if (notes.length === 0) {
@@ -20,28 +23,54 @@ window.onload = () => {
    }
 };
 
+// Opening add note form
 addButton.addEventListener("click", () => {
    addNoteContainer.style.top = "100px";
    addNoteForm.title.focus();
 });
 
+// Adding note to local storage
 addNoteForm.addEventListener("submit", (e) => {
    e.preventDefault();
-   notes.push({
-      id: noteID,
-      title: addNoteForm.title.value,
-      body: addNoteForm.body.value.replaceAll("\n", "<br>")
-   });
-   localStorage.setItem("notes", JSON.stringify(notes));
-   localStorage.setItem("noteID", ++noteID);
-   addNoteContainer.style.top = "-600px";
-   addToDOM(notes[notes.length - 1]);
-   noNotes.style.display = "none";
 
-   addNoteForm.title.value = "";
-   addNoteForm.body.value = "";
+   title = addNoteForm.title.value.trim();
+   body = addNoteForm.body.value.trim().replaceAll("\n", "<br>");
+
+   if (title === "" && body === "") {
+      titleError.style.display = "inherit";
+      bodyError.style.display = "inherit";
+   } else if (title === "") {
+      titleError.style.display = "inherit";
+      bodyError.style.display = "none";
+   } else if (body === "") {
+      bodyError.style.display = "inherit";
+      titleError.style.display = "none";
+   } else {
+      titleError.style.display = "none";
+      bodyError.style.display = "none";
+
+      // adding note to notes object
+      notes.push({
+         id: noteID,
+         title,
+         body
+      });
+
+      // updating local storage
+      localStorage.setItem("notes", JSON.stringify(notes));
+      localStorage.setItem("noteID", ++noteID);
+
+      // closing add note form
+      addNoteCloseButton.click();
+
+      // adding note to DOM
+      addToDOM(notes[notes.length - 1]);
+
+      noNotes.style.display = "none";
+   }
 });
 
+// Adding note to DOM
 function addToDOM(note) {
    notesConatiner.innerHTML += `
       <div class="note" id="note${note.id}">
@@ -54,16 +83,24 @@ function addToDOM(note) {
    `;
 };
 
+// Closing the Add note form
 addNoteCloseButton.addEventListener("click", () => {
    addNoteContainer.style.top = "-600px";
+
+   setTimeout(() => {
+      addNoteForm.title.value = "";
+      addNoteForm.body.value = "";
+
+      titleError.style.display = "none";
+      bodyError.style.display = "none";
+   }, 501);
 });
 
+// Deleting a note
 function deleteNote(id) {
-   console.log("id", id);  
    document.querySelector("#note" + id).remove();
    notes = notes.filter((note) => note.id !== id);
    localStorage.setItem("notes", JSON.stringify(notes));
-   console.log(notes);
    if (notes.length === 0) {
       noNotes.style.display = "flex";
    }
