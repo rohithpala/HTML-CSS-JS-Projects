c38cc8a4a378760f8c9cbbc497da7d55506e7f50
rohithpala <rohithpala02@gmail.com>
2023-03-08 15:47:09 +0530

    Login Signup Page

--- login-signup-page/script.js
+++ login-signup-page/script.js
@@ -4,18 +4,18 @@ const pwFields = document.querySelector(".password");
 
 console.log(Array.from(showHidePw));
 
-// showHidePw.forEach(icon => {
-//    icon.addEventListener("click", () => {
-//       pwFields.forEach(pwField => {
-//          if (pwField.type === "password") {
-//             pwField.type = "text";
-//             icon.classList.add("uil-eye-slash");
-//             icon.classList.remove("uil-eye");
-//          } else {
-//             pwField.type = "password";
-//             icon.classList.add("uil-eye");
-//             icon.classList.remove("uil-eye-slash");
-//          }
-//       });
-//    });
-// });
+showHidePw.forEach(icon => {
+   icon.addEventListener("click", () => {
+      pwFields.forEach(pwField => {
+         if (pwField.type === "password") {
+            pwField.type = "text";
+            icon.classList.add("uil-eye-slash");
+            icon.classList.remove("uil-eye");
+         } else {
+            pwField.type = "password";
+            icon.classList.add("uil-eye");
+            icon.classList.remove("uil-eye-slash");
+         }
+      });
+   });
+});
