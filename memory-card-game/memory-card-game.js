387b2161881133cc5e8047ff532dced02e5a410b
rohithpala <rohithpala02@gmail.com>
2023-03-08 13:35:54 +0530

    Memory Crad Game using HTML, CSS & JS

--- memory-card-game/memory-card-game.js
+++ memory-card-game/memory-card-game.js
@@ -8,14 +8,29 @@ const movesElement = document.querySelector(".moves");
 const finalMovesElement = document.querySelector(".final-moves");
 const minutesElement = document.querySelector(".minutes");
 const secondsElement = document.querySelector(".seconds");
+const finalTime = document.querySelector(".final-time");
 
 let closeImageElements, openImageElements;
-
 let noOfCards = 0;
+let clicks = 0, fixedImages = 0;
+let image1Index = -1, image2Index = -1;
 
 nCards.forEach((nCard) => {
    nCard.addEventListener("click", () => {
       noOfCards = parseInt(nCard.dataset.n);
+
+      if (noOfCards === 24) {
+         if (window.innerWidth > 500)
+            document.documentElement.style.setProperty('--cols', 6);
+         else
+            document.documentElement.style.setProperty('--cols', 4);
+      } else if (noOfCards === 20) {
+         if (window.innerWidth > 400)
+            document.documentElement.style.setProperty('--cols', 5);
+         else
+            document.documentElement.style.setProperty('--cols', 4);
+      }
+
       gameGrid.innerHTML = "";
 
       for (let i = 0; i < noOfCards; i++) {
@@ -42,7 +57,9 @@ nCards.forEach((nCard) => {
                movesElement.textContent = parseInt(movesElement.textContent) + 1;
 
                if (fixedImages === noOfCards - 2) {
+                  clearInterval(timerInterval);
                   finalMovesElement.textContent = movesElement.textContent;
+                  finalTime.textContent = `${minutesElement.textContent} minutes ${secondsElement.textContent} seconds`;
                   gameCompletedModal.style.display = "flex";
                }
 
@@ -84,10 +101,22 @@ nCards.forEach((nCard) => {
    });
 });
 
-let clicks = 0, fixedImages = 0;
-let image1Index = -1, image2Index = -1;
+let timerInterval, minutes, seconds;
+function startTimer() {
+   timerInterval = setInterval(() => {
+      seconds = parseInt(secondsElement.textContent);
+      minutes = parseInt(minutesElement.textContent);
+
+      if (seconds === 59) {
+         secondsElement.textContent = "00";
+         minutesElement.textContent = (minutes + 1).toString().padStart(2, "0");
+      }
+      secondsElement.textContent = (seconds + 1).toString().padStart(2, "0");
+   }, 1000);
+}
 
 function startGame() {
+   startTimer();
    const imageArray = Array.from({ length: 30 }, (_, i) => i);
    let randomImageIndex, randomImageValue;
 
@@ -120,6 +149,9 @@ function home() {
    gameContainer.style.display = "none";
    nCardsContainer.style.display = "flex";
 
+   clearInterval(timerInterval);
+   minutesElement.textContent = "00";
+   secondsElement.textContent = "00";
    movesElement.textContent = 0;
    clicks = 0;
    image1Index = -1;
@@ -135,6 +167,9 @@ function restart() {
       closeImageElements[index].style.display = "flex";
    });
 
+   clearInterval(timerInterval);
+   minutesElement.textContent = "00";
+   secondsElement.textContent = "00";
    movesElement.textContent = 0;
    clicks = 0;
    image1Index = -1;
